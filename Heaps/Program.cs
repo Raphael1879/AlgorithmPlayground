4b43f8245d5d32cae10acd1dec2f615083676484
Lord_Stamm <stamm.raphael17@gmail.com>
2026-06-09 19:25:22 +0200

    feat: impl basic min heap class

---
+++ Heaps/Program.cs
@@ -0,0 +1,154 @@
+﻿var testHeap = new MinHeap();
+var numbersToAdd = new int[] { 80, 70, 18, 30, 40 };
+
+foreach (int i in numbersToAdd) { 
+    testHeap.Add(i);
+}
+
+testHeap.PrintTree();
+
+var removedValue = testHeap.Pop();
+Console.WriteLine("Removed Value: " + removedValue);
+
+testHeap.PrintTree();
+
+class MinHeap
+{
+    public List<int> Heap = new List<int>();
+
+    public void Add(int value)
+    {
+        Heap.Add(value);
+        SiftUp(Heap.Count - 1);
+    }
+
+    public int Pop()
+    {
+        if (Heap.Count == 0)
+            throw new InvalidOperationException("Heap is empty");
+
+        var oldRoot = Heap[0];
+
+        if (Heap.Count == 1)
+        {
+            Heap.RemoveAt(0);
+            return oldRoot;
+        }
+
+        Heap[0] = Heap[Heap.Count - 1];
+        Heap.RemoveAt(Heap.Count - 1);
+
+        SiftDown(0);
+
+        return oldRoot;
+    }
+
+    public void SiftDown(int index) {
+
+        var currentIndex = index;
+
+        while(true)
+        {
+            var leftIndex = GetLeftIndex(currentIndex);
+            var rightIndex = GetRightIndex(currentIndex);
+
+            var smallest = currentIndex;
+
+            if(leftIndex < Heap.Count && Heap[leftIndex] < Heap[smallest])
+            {
+                smallest = leftIndex;
+            }
+
+            if(rightIndex < Heap.Count && Heap[rightIndex] < Heap[smallest])
+            {
+                smallest = rightIndex;
+            }
+
+            if(smallest == currentIndex)
+            {
+                break;
+            }
+
+            Swap(smallest, currentIndex);
+            currentIndex = smallest;
+        }
+    }
+
+    public void SiftUp(int index)
+    {
+
+        var currentIndex = index;
+
+        while (currentIndex > 0)
+        {
+
+            var parentIndex = GetParentIndex(index);
+
+            if (Heap[parentIndex] > Heap[index])
+            {
+
+                Swap(index, parentIndex);
+
+                currentIndex = parentIndex;
+
+            }
+            else
+            {
+                break;
+            }
+
+        }
+
+    }
+
+    private void Swap(int a, int b)
+    {
+        var temp = Heap[a];
+        Heap[a] = Heap[b];
+        Heap[b] = temp;
+    }
+
+    private int GetParentIndex(int index)=>(index - 1) / 2;
+    private int GetLeftIndex(int index) => (index * 2) + 1;
+    private int GetRightIndex(int index) => (index * 2) + 2;
+
+
+    //Cha- Gpt generierte Hilfsmethode für das anzeigen des Trees
+    public void PrintTree()
+    {
+        if (Heap.Count == 0)
+        {
+            Console.WriteLine("(empty heap)");
+            return;
+        }
+
+        int level = 0;
+        int index = 0;
+
+        while (index < Heap.Count)
+        {
+            int nodesInLevel = (int)Math.Pow(2, level);
+
+            // Some spacing to make it look tree-like
+            int leadingSpaces = (int)Math.Pow(2, Math.Max(0, 4 - level));
+
+            Console.Write(new string(' ', leadingSpaces));
+
+            for (int i = 0; i < nodesInLevel && index < Heap.Count; i++)
+            {
+                Console.Write($"{Heap[index],3}");
+
+                int betweenSpaces = (int)Math.Pow(2, Math.Max(0, 5 - level));
+                Console.Write(new string(' ', betweenSpaces));
+
+                index++;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            level++;
+        }
+    }
+
+}
--- IntroSort/Program.cs
+++ IntroSort/Program.cs
@@ -2,7 +2,7 @@
 using System.Diagnostics;
 
 //var originalData = new[] { 8, 3, 5, 1, 9, 2, 33, 78, 232, 2312, 1231, 213, 4, 64, 7657456, 45848, 465456, 4646, 34522, 7876989, 7866, 6444, 3322, 1, 1, 32, 54, 7 };
-var originalData = Enumerable.Range(1, 10000).ToArray();
+var originalData = Enumerable.Range(1, 10000).Reverse().ToArray(); //worst case for quickSort. introSort should switch to heap sort
 
 var introData = (int[])originalData.Clone();
 
@@ -12,7 +12,7 @@ sw1.Stop();
 
 Console.WriteLine("Intro Sort result:");
 Console.WriteLine(SortHelper.GetArrayString(introData, 0, 100));
-Console.WriteLine($"Intro Sort time: {sw1.ElapsedTicks} ticks");
+Console.WriteLine($"Intro Sort time: {sw1.ElapsedMilliseconds} ms");
 
 Console.WriteLine();
 
@@ -24,6 +24,6 @@ sw2.Stop();
 
 Console.WriteLine("QuickSort result:");
 Console.WriteLine(SortHelper.GetArrayString(quickData, 0, 100));
-Console.WriteLine($"QuickSort time: {sw2.ElapsedTicks} ticks");
+Console.WriteLine($"QuickSort time: {sw2.ElapsedMilliseconds} ms");
 
 Console.WriteLine("Done.");
